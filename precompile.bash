d25b6711e32e46c3cb3756e8da6d072266d38032
steven <chenglin@ont.com>
2019-01-28 15:01:14 +0800

    add script

---
+++ precompile.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+apt install cmake make automake libbz2-dev libssl-dev libgmp3-dev autotools-dev build-essential libicu-dev autoconf libtool curl zlib1g-dev doxygen graphviz
+
---
+++ prepare.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd ontdtcC_llvm/tools
+git clone https://github.com/llvm-mirror/clang
+git clone https://github.com/llvm-mirror/lld
