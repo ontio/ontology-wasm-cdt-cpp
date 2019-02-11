c57b1ed0de9fb10f1a5297b81ffdda4f3709739c
steven <chenglin@ont.com>
2019-02-11 18:30:07 +0800

    -nostdinc -static for default

--- example/test.cmd
+++ example/test.cmd
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ont_c -nostdinc -I/home/cl/github/ont.dtcC/install/include/libc -L/home/cl/github/ont.dtcC/install/lib -lc -lmalloc -static test_memset.c
+ont_c -I/home/cl/github/ont.dtcC/install/include/libc -L/home/cl/github/ont.dtcC/install/lib -lc -lmalloc test_memset.c
 cp a.out /home/cl/go/src/github.com/go-interpreter/wagon/cmd/wasm-run/
