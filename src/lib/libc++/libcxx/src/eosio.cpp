ef8246dacfe1bb8e5836be7aecbac1d1104ccfb0
steven <chenglin@ont.com>
2019-02-22 16:33:05 +0800

    fixed ont_assert link issue. this is tmp way

--- src/lib/libc++/libcxx/src/eosio.cpp
+++ src/lib/libc++/libcxx/src/eosio.cpp
@@ -5,6 +5,6 @@
  */
 
 extern "C" {
-   void ont_assert(unsigned int, const char*);
-   void __cxa_pure_virtual() { ont_assert(false, "pure virtual method called"); }
+   void ont_assert(unsigned int, const char*, unsigned int);
+   void __cxa_pure_virtual() { ont_assert(false, "pure virtual method called", 27); }
 }
--- src/lib/libc++/libcxx/src/locale.cpp
+++ src/lib/libc++/libcxx/src/locale.cpp
@@ -44,9 +44,9 @@
 #pragma clang diagnostic ignored "-Wsign-conversion"
 #endif
 
-extern "C" void ont_assert(bool, const char*);
-size_t strftime(char*, size_t, const char*, const struct tm*) { ont_assert(false, "strftime called"); }
-size_t strftime_l(char*, size_t, const char*, const struct tm*, locale_t) { ont_assert(false, "strftime_l called"); }
+extern "C" void ont_assert(bool, const char*, unsigned int);
+size_t strftime(char*, size_t, const char*, const struct tm*) { ont_assert(false, "strftime called",16 ); }
+size_t strftime_l(char*, size_t, const char*, const struct tm*, locale_t) { ont_assert(false, "strftime_l called", 18); }
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
