a1ebbea0fd9c44e65b86da64540f732277f48fd8
steven <chenglin@ont.com>
2019-02-28 15:12:38 +0800

    for rename git need rm old dir

--- install/include/ontio/public_key.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "crypto.hpp"
-
-// This file only exists so that existing contracts that include ontiolib/public_key.hpp do not break.
-// Going forward contracts should instead just include ontiolib/crypto.hpp
