f88f8e56dcd5b3d4befbf4282ca3fbdc62600798
steven <chenglin@ont.com>
2019-09-25 15:17:43 +0800

    make wasm call neo

--- install/include/ontiolib/callneo.hpp
+++ install/include/ontiolib/callneo.hpp
@@ -5,10 +5,11 @@ using std::array;
 namespace ontio {
 enum { 
 	ByteArrayType = 0x00,
-	AddressType = 0x01, 
-	BooleanType = 0x02,
-	IntType = 0x03,
-	H256Type = 0x04,
+	StringType = 0x01,
+	AddressType = 0x02, 
+	BooleanType = 0x03,
+	IntType = 0x04,
+	H256Type = 0x05,
 
 	ListType  = 0x10
 };
@@ -84,28 +85,15 @@ struct NeoBoolean {
 
 template<typename type>
 struct NeoInt {
-	static_assert( sizeof(type) <= sizeof(int128_t), "NeoInt suuport most sizeof(int128_t) size" );
 	type value;
 	
 	template<typename DataStream>
 	friend DataStream& operator<<( DataStream& ds, const NeoInt<type>& v ) {
 		uint8_t ttype = IntType;
-		uint8_t k = 0;
-		ds << ttype;
+		int128_t tv = int128_t(v.value);
 
-		if (v.value > 0) {
-			uint32_t size = (sizeof(type) + 1);
-			ds << size;
-			ds << v.value;
-			ds << k;
-		} else if (v.value == 0) {
-			uint32_t size = 0;
-			ds << size;
-		} else {
-			uint32_t size = (sizeof(type));
-			ds << size;
-			ds << v.value;
-		}
+		ds << ttype;
+		ds << tv;
 
 		return ds;
 	}
@@ -113,20 +101,11 @@ struct NeoInt {
 	template<typename DataStream>
 	friend DataStream& operator>>( DataStream& ds, NeoInt<type>& v ) {
 		uint8_t ttype;
-		uint32_t size;
+		int128_t tv;
 		ds >> ttype;
-		ds >> size;
+		ds >> tv;
 		check(ttype == IntType, "NeoInt type error");
-		check(size <= sizeof(v.value), "NeoInt size overflow");
-		if (size == 0) {
-			v.value = 0;
-		} else {
-			v.value = 0;
-			vector<char> res;
-			res.resize(size);
-			ds.read(res.data(), res.size());
-			std::copy(res.begin(), res.end(), ((uint8_t *)&(v.value)));
-		}
+		v.value = tv;
 		return ds;
 	}
 };
